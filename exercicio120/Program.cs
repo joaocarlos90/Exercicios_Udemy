3a33dedf8a0f81ec43bdb5871119726b54204214
Joao Carlos Alves <suporte_carlos@live.com>
2020-09-29 22:25:56 -0300

    exercicio 120 STRING BUILD

---
+++ exercicio120/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using exercicio120.Entities;
+
+namespace exercicio120
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip");
+            Comment c2 = new Comment("wow that´s awesome");
+            Post p1 = new Post(
+                DateTime.Now,
+                "Traveling to new Zealand",
+                "I´m going to visit this wonderful coun",
+                12);
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Good night");
+            Comment c4 = new Comment("may the force be with you");
+            Post p2 = new Post(
+                DateTime.Now,
+                "good night guys",
+                "see you tomorrow",
+                5);
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+        }
+    }
+}
